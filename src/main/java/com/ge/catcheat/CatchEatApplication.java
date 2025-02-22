68939154b47edd40fe90b4f59039e7db2206fc46
Euiseon <inia8inia@gmail.com>
2025-02-22 15:41:12 +0900

    Initial commit

---
+++ src/main/java/com/ge/catcheat/CatchEatApplication.java
@@ -0,0 +1,13 @@
+package com.ge.catcheat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchEatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatchEatApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ge/catcheat/CatchEatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ge.catcheat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchEatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
